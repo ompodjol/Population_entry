126a6b63ab92f54bb8dacb30495a6ca28e350c3c
jollyjae <jollyjae.ompod@tieto.com>
2021-04-12 09:17:45 +0200

    test 2

--- main.cpp
+++ main.cpp
@@ -3,35 +3,87 @@
 using namespace std;
 
 class YouTubeChannel {
-public:
+private:
 	string Name;
-	string OwnerName;
 	int SubscribersCount;
 	list<string> PublishedVideoTitles;
+protected:
+	string OwnerName;
+	int ContentQuality;
 
+public:
 	YouTubeChannel(string name, string ownerName) {
 		Name = name;
 		OwnerName = ownerName;
 		SubscribersCount = 0;
+		ContentQuality = 0;
 	}
-
 	void getInfo() {
 		cout << "Name :" << Name << endl;
-		cout << "OwnerName :" << Name << endl;
-		cout << "SubscribersCount :" << Name << endl;
-		cout << "Videos :" << Name << endl;
+		cout << "OwnerName :" << OwnerName << endl;
+		cout << "SubscribersCount :" << SubscribersCount << endl;
+		cout << "Videos :" << endl;
 		for (string videoTitle : PublishedVideoTitles) {
 			cout << videoTitle << endl;
 		}
+	}
+	void Subscribe() {
+		SubscribersCount++;
+	}
+	void UnSubscribe() {
+		if(SubscribersCount>0)
+			SubscribersCount--;
+	}
+	void PublishedVideo(string title) {
+		PublishedVideoTitles.push_back(title);
+	}
+	void CheckAnalytics() {
+		if (ContentQuality < 5)
+			cout << Name << " has bad quality content." << endl;
+		else
+			cout << Name << " has great quality content." << endl;
+	}
+};
+
+class CookingYouTubeChannel :public YouTubeChannel {
+public:
+	CookingYouTubeChannel(string name, string ownerName):YouTubeChannel(name, ownerName) {
+
+	}
+	void Practice() {
+		cout << OwnerName <<" practicing cooking, learning new recipes, experimenting with spices..." << endl;
+		ContentQuality++;
+	}
+};
+
+class SingersYouTubeChannel : public YouTubeChannel {
+public:
+	SingersYouTubeChannel(string name, string ownerName) :YouTubeChannel(name, ownerName) {
+
+	}
+	void Practice() {
+		cout << OwnerName << " is taking singing classes, learning new songs, learning how to dance..." << endl;
+		ContentQuality++;
 	}
 };
 
 int main()
 {
-	YouTubeChannel ytChannel("CodeBeauty", "Jolly");
-	ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
+	CookingYouTubeChannel cookingYtChannel("Amy's Kitchen", "Amy");
+	SingersYouTubeChannel singersYtChannel("John's SingerBar", "John");
 
-	ytChannel.getInfo();
+	cookingYtChannel.Practice();
+	singersYtChannel.Practice();
+	singersYtChannel.Practice();
+	singersYtChannel.Practice();
+	singersYtChannel.Practice();
+	singersYtChannel.Practice();
+
+	YouTubeChannel* yt1 = &cookingYtChannel;
+	YouTubeChannel* yt2 = &singersYtChannel;
+
+	yt1->CheckAnalytics();
+	yt2->CheckAnalytics();
 
 	system("pause>0");
 }
